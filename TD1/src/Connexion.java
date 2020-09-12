b3cae36450ab92c49bf56c94808696c712ed89e4
orliange2u <orliange2u@iutm-inf-f36-10.iutmetz.site.univ-lorraine.fr>
2020-09-12 11:31:55 +0200

    Correction de l'init (mise en forme)

--- TD1/src/Connexion.java
+++ TD1/src/Connexion.java
@@ -1,16 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connexion {
 	public Connection creeConnexion() {
-		String url =
-		"jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/etud1u_base";
+		String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/etud1u_base";
 		String login = "etud1u_appli";
 		String pwd = "pass_etud1u";
+
 		Connection maConnexion = null;
+
 		try {
-		maConnexion = DriverManager.getConnection(url, login, pwd);
+			maConnexion = DriverManager.getConnection(url, login, pwd);
 		} catch (SQLException sqle) {
-		System.out.println("Erreur connexion" + sqle.getMessage());
+			System.out.println("Erreur connexion" + sqle.getMessage());
 		}
+
 		return maConnexion;
-		 }
+	}
 }
